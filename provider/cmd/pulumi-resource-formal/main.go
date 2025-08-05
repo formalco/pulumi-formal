4311fe13ff0dab243c5e1b8022f29eaa547131b0
Eric L <eric@joinformal.com>
2025-08-05 10:25:29 -0700

    Run setup.sh

--- provider/cmd/pulumi-resource-formal/main.go
+++ provider/cmd/pulumi-resource-formal/main.go
@@ -19,8 +19,8 @@ import (
 
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 
-	xyz "github.com/pulumi/pulumi-xyz/provider"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
+	formal "github.com/formalco/pulumi-formal/provider"
+	"github.com/formalco/pulumi-formal/provider/pkg/version"
 )
 
 //go:embed schema.json
@@ -28,5 +28,5 @@ var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
-	tfbridge.Main("xyz", version.Version, xyz.Provider(), pulumiSchema)
+	tfbridge.Main("formal", version.Version, formal.Provider(), pulumiSchema)
 }
